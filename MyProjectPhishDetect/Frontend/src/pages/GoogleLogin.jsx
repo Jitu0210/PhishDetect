6d6bd303e33ce261094beb3c9a93c183f8725099
jitu <jitukumar63766@gmail.com>
2025-05-05 20:50:29 +0530

    Added Oauth file to improve login experience

--- MyProjectPhishDetect/Frontend/src/pages/GoogleLogin.jsx
+++ MyProjectPhishDetect/Frontend/src/pages/GoogleLogin.jsx
@@ -1,20 +1,11 @@
-import "react";
-import { Oauth2 } from 'googleapis/build/src/apis/oauth2/v2';
+import 'react'
 
-
-const GoogleLogin = userG() 
-
-  const googlelogin = useGoogleLogin({
-
-  })
+const GoogleLogin = () => {
   return (
-    <div className="flex justify-center item-center mt-9">
-      <div>
-        <button onClick={googlelogin} className="px-4 py-2 bg-blue-500  text-white rounded-lg hover:bg-blue-600 transition duration-200 focus:outline-none focus:ring-2 focus:ring-blue-300">
-          Login with Google
-        </button>
-      </div>
-    </div>
-  );
+    <div>
 
-export default GoogleLogin;
+    </div>
+  )
+}
+
+export default GoogleLogin
